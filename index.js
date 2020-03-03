17d1c16f1812aadc1290f182e47407cb6e565ae7
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2020-03-03 11:01:23 +0100

    Add practice for variables

---
+++ index.js
@@ -0,0 +1,24 @@
+// Variables
+
+var personNameVar = 'isidor'; // Old way
+let personNameLet = 'ivan'; // New way
+const personNameConst = 'radovan'; // Constant variables
+
+personNameConst = 'pera'; // Cannot be re-assigned.
+
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
+console.log('Hello, ' + personNameConst);
