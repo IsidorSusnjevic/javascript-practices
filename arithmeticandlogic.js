e06719ba4d2faa437c0568f5448c9a15bd666db4
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2020-03-03 11:56:23 +0100

    Add example for arithmetic and logic practice

--- arithmeticandlogic.js
+++ arithmeticandlogic.js
@@ -1,7 +1,14 @@
-// Arithmetics
-let wall1 = 100;
-let wall2 = 100;
-let surface = wall1 * wall2;
-let result = surface + 34534 + 4385 - 54123 * 6 / 34 - surface;
+let randomNumberOne = 1313
+let randomNumberTwo = 9847 
+let result = randomNumberOne + randomNumberTwo * 12
 
 console.log(result);
+
+let logic = 3 = 3;
+let flogic = 4 > 3;
+
+
+let isidorIsOlderThan18 = true;// isidor.age > 18
+let ivanIsOlderThan18 = true;// ivan.age > 18
+
+let theyCanBothDrink = isidorIsOlderThan18 && ivanIsOlderThan18;
