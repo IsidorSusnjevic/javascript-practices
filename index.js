a656c214d4af25f152e2ac43cce292d6298ec501
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2020-03-03 11:11:34 +0100

    Add example with backticks

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var personNameVar = 'isidor'; // Old way
 let personNameLet = 'ivan'; // New way
 const personNameConst = 'radovan'; // Constant variables
 
-personNameConst = 'pera'; // Cannot be re-assigned.
+// personNameConst = 'pera'; // Cannot be re-assigned.
 
+// It's absolutely the same thing with "double quotes" as with 'single quotes'
+console.log('Hello, ' + personNameConst);// String concatenation
 console.log('Hello, ' + personNameConst);
 console.log('Hello, ' + personNameConst);
 console.log('Hello, ' + personNameConst);
@@ -21,4 +23,9 @@ console.log('Hello, ' + personNameConst);
 console.log('Hello, ' + personNameConst);
 console.log('Hello, ' + personNameConst);
 console.log('Hello, ' + personNameConst);
-console.log('Hello, ' + personNameConst);
+
+// With backticks
+console.log(`Hello, hello, ${personNameConst}`);
+console.log(`Hello, hello, ${personNameConst}`);
+console.log(`Hello, hello, ${personNameConst}`);
+console.log(`Hello, hello, ${personNameConst}`);
