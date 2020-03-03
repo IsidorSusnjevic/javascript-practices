980cfe3c7b6c89d1bd90da6c0930b8401b95cbbd
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2020-03-03 12:06:58 +0100

    Improved arithmetic and logic example

--- arithmeticandlogic.js
+++ arithmeticandlogic.js
@@ -1,14 +1,36 @@
-let randomNumberOne = 1313
-let randomNumberTwo = 9847 
-let result = randomNumberOne + randomNumberTwo * 12
+// Result of arithmetic expressions are numbers.
+// Result of logic expressions are booleans.
+// Results of arithmetic and logic expressions can be assigned to variables.
+// Results of both expressions are calculated before assigning to a variable.
+
+// Arithmetics
+let wall1 = 100;
+let wall2 = 100;
+let surface = wall1 * wall2;
+let result = surface + 34534 + 4385 - 54123 * 6 / 34 - surface;
 
 console.log(result);
 
-let logic = 3 = 3;
-let flogic = 4 > 3;
-
+// Logic
+let logicResult = 2 > 3; // false
+logicResult = 2 < 3; // true
 
 let isidorIsOlderThan18 = true;// isidor.age > 18
 let ivanIsOlderThan18 = true;// ivan.age > 18
 
-let theyCanBothDrink = isidorIsOlderThan18 && ivanIsOlderThan18;
+let theyCanBothDrink = isidorIsOlderThan18 && ivanIsOlderThan18; // true 
+
+// Arithmetic operations: +, -, *, /
+// Logic operations: &&, ||, !
+
+
+!true; // false
+true && true; // true
+true && false; // false
+false && true; // false
+false || true; // true
+true || false; // true
+true || true; // true
+
+
+!((true && false) || (!false && false) && true); // this will return true
